10244b3b1dd7c4b6a420c6c86182f0359a58baaa
Robin <robin.kertels@outlook.com>
2018-06-22 03:30:21 +0200

    Slight cleanup

--- app/src/main/java/k0bin/notes/ui/fragment/NotesFragment.java
+++ app/src/main/java/k0bin/notes/ui/fragment/NotesFragment.java
@@ -1,10 +1,7 @@
 package k0bin.notes.ui.fragment;
 
-import android.arch.lifecycle.ViewModelProvider;
 import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
 import android.os.Bundle;
-
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,30 +9,20 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import k0bin.notes.R;
-import k0bin.notes.model.Note;
 import k0bin.notes.ui.adapter.NotesAdapter;
 import k0bin.notes.viewModel.NotesViewModel;
 
 
 /**
- * A simple {@link Fragment} subclass.
- * Use the {@link NotesFragment#newInstance} factory method to
- * create an instance of this fragment.
+ * Fragment that lists all notes
  */
 public class NotesFragment extends Fragment {
 	private NotesAdapter adapter;
-	private List<Note> notes;
 	private NotesViewModel viewModel;
 
-	public NotesFragment() {
-		// Required empty public constructor
-	}
+	public NotesFragment() {}
 
 	@Override
 	public void onActivityCreated(@Nullable Bundle savedInstanceState) {
@@ -48,8 +35,6 @@ public class NotesFragment extends Fragment {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-
-		notes = new ArrayList<>();
 	}
 
 	@Override
@@ -69,28 +54,6 @@ public class NotesFragment extends Fragment {
 
 		view.findViewById(R.id.fab).setOnClickListener(it -> {
 			//Navigation.findNavController(it).
-			notes.add(new Note(0, "hi", "hiiii"));
-			adapter.submitList(notes);
 		});
-	}
-
-	@Override
-	public void onAttach(Context context) {
-		super.onAttach(context);
-	}
-
-	@Override
-	public void onDetach() {
-		super.onDetach();
-	}
-
-	/**
-	 * Use this factory method to create a new instance of
-	 * this fragment using the provided parameters.
-	 *
-	 * @return A new instance of fragment NotesFragment.
-	 */
-	public static NotesFragment newInstance() {
-		return new NotesFragment();
 	}
 }
